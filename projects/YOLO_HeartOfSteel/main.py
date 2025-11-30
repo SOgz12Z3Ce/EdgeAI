46dad156b28840440d7f8d83acd6d523c8948f5f
SOgz12Z3Ce <sogz12z3ce@gmail.com>
2025-11-30 16:02:57 +0800

    feat: try yolo11l.pt

--- projects/YOLO_HeartOfSteel/main.py
+++ projects/YOLO_HeartOfSteel/main.py
@@ -1,5 +1,5 @@
 from ultralytics import YOLO
 
 if __name__ == '__main__':
-    model = YOLO("yolo11n.yaml")
-    results = model.train(data='./train.yaml', epochs=24, batch=24, pretrained=False)
+    model = YOLO("yolo11l.pt")
+    results = model.train(data='./train.yaml', project='.', epochs=24, batch=24, pretrained=False)
